74f5cd5640dbd77452417a6e30f8f46458f00e42
zhangkai <zhangkai.gis@163.com>
2024-07-03 10:44:14 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+docker version 
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose version
+wget https://go.dev/dl/go1.20.10.linux-amd64.tar.gz
+sudo tar -zxvf go1.20.10.linux-amd64.tar.gz -C /usr/local/
+echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee /etc/profile.d/go.sh
+source /etc/profile.d/go.sh
+go version
